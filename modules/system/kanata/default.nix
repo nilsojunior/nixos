f30b89eb569a89b40098540bb82696ea18b945f7
Nilso Júnior <162613094+nilsojunior@users.noreply.github.com>
2026-09-01 21:46:59 -0300

    [Kanata] Use `tap-hold-press` and update layout

--- modules/system/kanata/default.nix
+++ modules/system/kanata/default.nix
@@ -23,20 +23,20 @@ in
     tab    q    w    e    r    t    y    u    i    o    p    [    ]   \
     caps   a    s    d    f    g    h    j    k    l    ;    '    ret
     lsft < z    x    c    v    b    n    m    ,    .    /    rsft      up
-    lctl   lmet lalt           spc            ralt rmet rctl      left down rght
+    lctl   lmet lalt           spc            ralt ro          left down rght
 )
 
 (defalias
-    escctrl (tap-hold 150 150 esc lctrl)
+    esctrl (tap-hold-press 150 150 esc lctrl)
 )
 
 (deflayer qwerty
   esc        f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12
-  grv        1    2    3    4    5    6    7    8    9    0    \    =   bspc
-  tab        q    w    e    r    t    y    u    i    o    p    [    ]   \
-  @escctrl   a    s    d    f    g    h    j    k    l    -    '    ret
-  lsft     ; z    x    c    v    b    n    m    ,    .    /    rsft          up
-  lctl       lmet lalt           spc            ralt rmet rctl          left down rght
+  \          1    2    3    4    5    6    7    8    9    0    -    =   bspc
+  tab        q    w    e    r    t    y    u    i    o    p    [    ]   grv
+  @esctrl    a    s    d    f    g    h    j    k    l    ;    '    ret
+  lsft   esc z    x    c    v    b    n    m    ,    .    /    rsft          up
+  lctl       lmet lalt           spc            ralt rmet               left down rght
 )
                 '';
             };
